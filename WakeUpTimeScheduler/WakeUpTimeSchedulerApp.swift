b6f7082ebc12c34151d868e6d655776df029af8e
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2024-05-21 12:37:21 -0700

    Initial Commit

---
+++ WakeUpTimeScheduler/WakeUpTimeSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpTimeSchedulerApp.swift
+//  WakeUpTimeScheduler
+//
+//  Created by Amulya Gangam on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpTimeSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
